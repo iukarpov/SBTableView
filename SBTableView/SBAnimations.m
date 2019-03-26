dde700cd8ee44f7dc792b5f6d9e2559a84fbc062
iukarpov <spangegoth@gmail.com>
2019-03-26 15:09:45 +0300

    animation added

--- SBTableView/SBAnimations.m
+++ SBTableView/SBAnimations.m
@@ -17,10 +17,8 @@
     [shake setDuration:0.1];
     [shake setRepeatCount:3];
     [shake setAutoreverses:YES];
-    [shake setFromValue:[NSValue valueWithCGPoint:
-                         CGPointMake(cell.center.x - 5,cell.center.y)]];
-    [shake setToValue:[NSValue valueWithCGPoint:
-                       CGPointMake(cell.center.x + 5, cell.center.y)]];
+    [shake setFromValue:[NSValue valueWithCGPoint: CGPointMake(cell.center.x - 5,cell.center.y)]];
+    [shake setToValue:[NSValue valueWithCGPoint: CGPointMake(cell.center.x + 5, cell.center.y)]];
     [cell.layer addAnimation:shake forKey:@"position"];
 }
 
@@ -29,7 +27,6 @@
     cell.alpha = 0;
     CATransform3D transform = CATransform3DTranslate(CATransform3DIdentity, -250, 20, 0);
     cell.layer.transform = transform;
-    
     [UIView animateWithDuration:1.0 animations:^{
         cell.alpha = 1.0;
         cell.layer.transform = CATransform3DIdentity;
